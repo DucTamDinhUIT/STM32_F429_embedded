e7e1ff70ba64e4873a0783917c645cd1298c2390
Đinh Đức Tâm <ductam.astro@gmail.com>
2019-10-11 03:51:48 +0700

    Create java.java

---
+++ java.java
@@ -0,0 +1,15 @@
+import java.util.Random;
+
+public class java {
+/**
+* @param args
+*/
+public static void main(String[] args) {
+ // TODO Auto-generated method stub
+ Random rd=new Random();
+ int a=rd.nextInt(26)+25;
+ System.out.println("Random from 25 to 50 là "+" "+a);
+ int b=rd.nextInt((26)+(-10));
+ System.out.println("Random from -10 to 15 là :"+" " +b);
+}
+}
