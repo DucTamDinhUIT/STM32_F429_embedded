3ab818d4ee2585f62e77dd8dc3c4e4307a8db4b9
Đinh Đức Tâm <ductam.astro@gmail.com>
2019-10-11 04:14:21 +0700

    Create Bai1.java

---
+++ lab/bai1/src/com/bai1/Bai1.java
@@ -0,0 +1,15 @@
+package com.bai1;
+       
+import java.util.Random;
+
+public class Bai1 {
+
+    public static void main(String[] args) {
+     // TODO Auto-generated method stub
+     Random rd=new Random();
+     int a=rd.nextInt(26)+25;
+     System.out.println("ngau nhien from 25 to 50 là "+" "+a);
+     int b=rd.nextInt((26)+(-10));
+     System.out.println("Random from -10 to 15 là :"+" " +b);
+    }
+}
