bc933a006e6280d46d44918f8e8598863264bc71
Đinh Đức Tâm <ductam.astro@gmail.com>
2019-10-11 04:29:53 +0700

    Update java.java

--- java.java
+++ java.java
@@ -8,7 +8,7 @@ public class java {
      // TODO Auto-generated method stub
      Random rd=new Random();
      int a=rd.nextInt(26)+25;
-     System.out.println("ngau nhien from 25 to 50 là "+" "+a);
+     System.out.println("ntestttttt 25 to 50 là "+" "+a);
      int b=rd.nextInt((26)+(-10));
      System.out.println("Random from -10 to 15 là :"+" " +b);
     }
