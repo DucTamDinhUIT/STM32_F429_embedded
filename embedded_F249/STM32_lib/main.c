973f03014e872ca044eecdbe98edd5c49923b691
Đinh Đức Tâm <ductam.astro@gmail.com>
2019-11-14 14:38:17 +0700

    Update main.c

--- embedded_F249/STM32_lib/main.c
+++ embedded_F249/STM32_lib/main.c
@@ -19,20 +19,20 @@ void RCC_PLL_ENABLE()
 		RCC->AHB1RSTR		= 0x00000000u;
 	}
 	
-
-
-void GPIOG()
-	{	
-		PORTG->MODER			= 0x14000000u; 		//output
+void GPIO_Init()
+	{
+		RCC->AHB1ENR |= (1 << 6);								//GPIOG enable
+		RCC->AHB1ENR |= (1 << 0);								//GPIOA enable
+		PORTG->MODER |= (1 << 26);											//PORT13 pin26,27 output
+		PORTG->MODER &= ~(1 << 27);										//pin13 = "01" => bit [26;27] = 01 
+		PORTG->MODER |= (1 << 28);											//PORT13 pin28,29 output
+		PORTG->MODER &= ~(1 << 29);										//pin14 = "01" => bit [28;29] = 01
+		PORTA->MODER &= ~(1 << 0);		
 		PORTG->OTYPER			= 0x10000000u;		//PG13 push pull
 		PORTG->OSPEEDR		= 0x14000000u;	  //Output speed
 		PORTG->PUPDR			= 0x28000000u;		//pull down
-	}
-
-void GPIOA()
-	{
-		PORTA->MODER			= 0xA8000000u; 		//PA0 input
-		PORTA->PUPDR			= 0x64000001u;		//pull up
+		PORTA->PUPDR			= 0x64000002u;		//pull up
+		
 	}
 
 void ToggleLed()
@@ -43,21 +43,7 @@ void ToggleLed()
 int main(void) 
 {
 	RCC_PLL_ENABLE();
-	RCC->AHB1ENR |= (1 << 6);								//GPIOG enable
-	RCC->AHB1ENR |= (1 << 0);								//GPIOA enable
-
-
-	PORTG->MODER |= (1 << 26);											//PORT13 pin26,27 output
-	PORTG->MODER &= ~(1 << 27);										//pin13 = "01" => bit [26;27] = 01 
-	PORTG->MODER |= (1 << 28);											//PORT13 pin28,29 output
-	PORTG->MODER &= ~(1 << 29);										//pin14 = "01" => bit [28;29] = 01
-	PORTA->MODER &= ~(1 << 0);
-	
-	PORTG->OTYPER			= 0x10000000u;		//PG13 push pull
-	PORTG->OSPEEDR		= 0x14000000u;	  //Output speed
-	PORTG->PUPDR			= 0x28000000u;		//pull down
-	PORTA->PUPDR			= 0x64000002u;		//pull up
-
+	GPIO_Init();
 	
 	PORTG->ODR |= (1 << 13);						//bit 1 = output to pin 13 
 	PORTG->ODR |= (1 << 14);						//bit 1 = output to pin 14
